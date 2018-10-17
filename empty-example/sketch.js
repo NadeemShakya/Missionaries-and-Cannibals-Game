a4a649228ea1c4d3229799290740ca8c8af93e2d
Nadeem <shakya.nadim12@gmail.com>
2018-10-17 16:46:57 +0545

    State Generation for achieving the goal state.

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,42 +1,45 @@
 let initialState = [3, 3, 1];
-let goalState = [2, 2, 0];
+let goalState = [0, 0, 0];
 let state = [];
 let killedState = [];
-class CreateState {
+let iterator = true;
+class CreateState { 
   constructor() {
     this.value;
     this.parent;
+    this.visited;
   }
 }
 // Creating a root node.
 var  rootNode = new CreateState();
 rootNode.value = initialState;
 rootNode.parent = initialState;
+rootNode.visited = false;
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1000, 600);
   background(0);
+  stroke(255);
+  textSize(36);
+  fill(255);
+  text("PRESS F12 OR Ctrl+Shift+I and view the console tab.", 50, 50);
   state.push(rootNode);
-  for(let i = 0; i < state.length; i++) {
-  }
-  applyOperation(state[0]);
-  // applyOperation(state[3]);
-
+  while(iterator) {
+    applyOperation(state[state.length - 1])
+  }    
 }
 
 function applyOperation(tempState) {
+    if(tempState.visited === true) {
+      state.splice(state.length - 1, 1);
+    }else {
+    tempState.visited = true;
     boatPosition = tempState.value[2];
     // If Boat is at the left bank
     if(boatPosition === 1) {   
-      console.log("boat is going from Left to Right"); 
+      // console.log("boat is going from Left to Right"); 
       if(tempState.value[0] >= 1) {
         addState(tempState, [tempState.value[0] - 1, tempState.value[1] - 0, 0]);
-      }
-      if(tempState.value[1] >= 1) {
-        addState(tempState, [tempState.value[0] - 0, tempState.value[1] - 1, 0]);
-      }
-      if(tempState.value[0] >= 1 && tempState.value[1] >= 1) {
-        addState(tempState, [tempState.value[0] - 1, tempState.value[1] - 1, 0]);
       }
       if(tempState.value[0] >= 2) {
         addState(tempState, [tempState.value[0] - 2, tempState.value[1] - 0, 0]);
@@ -44,41 +47,67 @@ function applyOperation(tempState) {
       if(tempState.value[1] >= 2) {
         addState(tempState, [tempState.value[0] - 0, tempState.value[1] - 2, 0]);
       }
+      if(tempState.value[0] >= 1 && tempState.value[1] >= 1) {
+        addState(tempState, [tempState.value[0] - 1, tempState.value[1] - 1, 0]);
+      }
+      if(tempState.value[1] >= 1) {
+        addState(tempState, [tempState.value[0] - 0, tempState.value[1] - 1, 0]);
+      }            
     } else if(boatPosition === 0) {
       // If Boat is at the right bank.
-      console.log("boat is going from Right to Left") 
-      if(tempState.parent[0] - tempState.value[0] >= 1) {
+      // console.log("boat is going from Right to Left") 
+      if(initialState[0] - tempState.value[0] > 0) {
         addState(tempState, [tempState.value[0] + 1, tempState.value[1] + 0, 1]);
       }
-      if(tempState.parent[1] - tempState.value[1] >= 1) {
+      if(initialState[1] - tempState.value[1] > 0) {
         addState(tempState, [tempState.value[0] + 0, tempState.value[1] + 1, 1]);
       }
-      if((tempState.parent[0] - tempState.value[0] >= 1) && (tempState.parent[1] + tempState.value[1] >= 1)) {
+      if((initialState[0] - tempState.value[0] > 0) && (initialState[1] - tempState.value[1] > 0)) {
         addState(tempState, [tempState.value[0] + 1, tempState.value[1] + 1, 1]);
       }
-      if(tempState.parent[0] - tempState.value[0] >= 2) {
+      if(initialState[0] - tempState.value[0] > 2) {
         addState(tempState, [tempState.value[0] + 2, tempState.value[1] + 0, 1]);
       }
-      if(tempState.parent[1] - tempState.value[1] >= 2) {
+      if(initialState[1] - tempState.value[1] > 2) {
         addState(tempState, [tempState.value[0] + 0, tempState.value[1] + 2, 1]);
       }
     }
-    console.log(state);
-
+    
+  }
 }
+console.log("These are the states to successfully complete the game.");
+console.log(state);
+console.log("These are all the killedStates that don't lead to the goal state.");
+console.log(killedState); 
 
 function addState(parent, value) {
   var temp = new CreateState();
   temp.value = value;
   temp.parent = parent.value;
-  console.log( temp.value);
-  console.log( goalState);
-  if(goalState[0] === temp.value[0] && goalState[1] === temp.value[1]) {
-    console.log("win");
-  }
-  if(temp.value[0] >= temp.value[1]) {
+  temp.visited = false;
+  if(goalState[0] === value[0] && goalState[1] === value[1]) {
     state.push(temp);
+    iterator = false;
+    console.log("HURAH! THE GOAL STATE IS ACHIEVED");
+  }else if((temp.value[0] === 0) || temp.value[0] >= temp.value[1]) {
+    if((3 - temp.value[0] === 0) || (3 - temp.value[0] >= 3 - temp.value[1])){
+      if(repetitionChecker(value)) {
+        killedState.push(temp);
+      } else {
+        state.push(temp);
+      }
+    }
   }else if(temp.value[0] < temp.value[1]) {
     killedState.push(temp); 
   }
+}
+
+// Function to check whether a state already exists or not in the array
+function repetitionChecker(value) {
+  for(let i = 0; i < state.length; i++) {
+    if(state[i].value[0] === value[0] && state[i].value[1] === value[1] && state[i].value[2] === value[2]) {
+      return true;
+    }
+  }
+  return false;
 }
