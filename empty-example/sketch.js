e0ef928223b3d325dddd21c5db1b3d0bd10d4d30
Nadeem <shakya.nadim12@gmail.com>
2018-10-20 09:40:05 +0545

    code fixes

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -23,7 +23,6 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
   stroke(255);
-  textSize(36);
   fill(255);
   // text("PRESS F12 OR Ctrl+Shift+I and view the console tab.", 50, 50);
   rootNode.x = windowWidth / 2;
@@ -100,7 +99,7 @@ function addState(parent, value) {
   if(goalState[0] === value[0] && goalState[1] === value[1]) {
     state.push(temp);
     iterator = false;
-    // console.log("HURAH! THE GOAL STATE IS ACHIEVED");
+    console.log("HURAH! THE GOAL STATE IS ACHIEVED");
   }else if((temp.value[0] === 0) || temp.value[0] >= temp.value[1]) {
     if((3 - temp.value[0] === 0) || (3 - temp.value[0] >= 3 - temp.value[1])){
       if(repetitionChecker(value)) {
@@ -132,16 +131,13 @@ function displayState() {
   fill(255, 165, 0);
   noStroke();
   text(state[0].value, windowWidth/2, 70);
-  textSize(14);
+  textSize(16);
   for(i = 0; i < state.length; i++) {
     let tempArray = [];
     for(j = i + 1; j < state.length; j++) {
       if(state[j].parent[0] === state[i].value[0] && state[j].parent[1] === state[i].value[1] && state[j].parent[2] === state[i].value[2] ) {
-        // console.log(state[j].value);
-        // text(state[i].value, windowWidth / 2, 50 + i * 40);
         if(!tempChecker(state[j].value, tempArray)) {
           tempArray.push(state[j].value);
-          
         }
       }
     }
@@ -153,49 +149,43 @@ function displayState() {
         }
       } 
     }    
-
-    // console.log(tempArray  );
     if(tempArray.length === 1) {
-      // for(let i = 0; i < state.length; i++) {
-      //   if(state[i].value === tempArray[0][0] && state[i].value === tempArray[0][1] && state[i].value === tempArray[0][2]) {
-      //     fill(0, 255, 0);
-      //     text(tempArray[0], state[i].x, state[i].y + 40);
-      //   }else {
-      //     text(tempArray[0], state[i].x, state[i].y + 40);
-      //   }
-      // }
-      text(tempArray[0], state[i].x, state[i].y + 40);
+      for(let w = 0; w < state.length; w++) {
+        if(state[w].value[0] === tempArray[0][0] && state[w].value[1] === tempArray[0][1] && state[w].value[2] === tempArray[0][2]) {
+          if(state[w].visited) {
+            fill(0, 255, 0);            
+          }
+        }
+      }
+      stroke(255, 255, 255, 120);
 
+      line(state[i].x + 15, state[i].y, state[i].x + 15, state[i].y + 20);
+      noStroke();
+      text(tempArray[0], state[i].x, state[i].y + 40);
+      fill(255, 0, 0);
       for(let b = 0; b < state.length; b++) {
         if(state[b].value[0] === tempArray[0][0] && state[b].value[1] === tempArray[0][1] && state[b].value[2] === tempArray[0][2]) {
           state[b].x =  state[i].x;
           state[b].y = state[i].y + 50;
-
         }
       }       
     }else if(tempArray.length !== 0 && tempArray.length % 2 === 0) {
       for(p = 0; p < tempArray.length; p++) {
-        // for(let q = 0; q < state.length; q++) {
-        //   if(state[q].value[0] === tempArray[p][0] && state[q].value[1] === tempArray[p][1] && state[q].value[2] === tempArray[p][2]) {
-        //     console.log("bhayeko" + tempArray[p]);
-        //     if(state[q].visited === true) {
-        //       fill(0, 255, 0);
-        //       text(tempArray[p],(state[i].x - (25 * (tempArray.length - 1))) + p * 50, state[i].y + 40);
-        //     }else {
-        //       // fill(0, 0, 255);  
-        //       // text(tempArray[p],(state[i].x - (25 * (tempArray.length - 1))) + p * 50, state[i].y + 40);
-        //     }  
-        //   }
-        // }
-
-        // for(let t = 0; t < killedState.length; t++) {
-        //   if(killedState[t].value[0] === tempArray[p][0] && killedState[t].value[1] === tempArray[p][1] && killedState[t].value[2] === tempArray[p][2]){
-        //     fill(255, 0, 0);
-        //     text(tempArray[p],(state[i].x - (25 * (tempArray.length - 1))) + p * 50, state[i].y + 40);
-        //   }
-        // }
-        // fill(255, 165, 0);  
+        for(let q = 0; q < state.length; q++) {
+          if(state[q].value[0] === tempArray[p][0] && state[q].value[1] === tempArray[p][1] && state[q].value[2] === tempArray[p][2]) {
+            console.log("bhayeko" + tempArray[p]);
+            if(state[q].visited === true) {
+              fill(0, 255, 0);
+            }else { 
+              fill(0, 0, 255);  
+            }  
+          }
+        }
+        stroke(255, 255, 255, 120);
+        line(state[i].x + 15, state[i].y, state[i].x - (25 * (tempArray.length - 1)) + p * 50 + 15, state[i].y + 20);
+        noStroke();
         text(tempArray[p],(state[i].x - (25 * (tempArray.length - 1))) + p * 50, state[i].y + 40);
+        fill(255, 0, 0);
 
         for(let b = 0; b < state.length; b++) {
           if(state[b].value[0] === tempArray[p][0] && state[b].value[1] === tempArray[p][1] && state[b].value[2] === tempArray[p][2]) {
@@ -206,22 +196,21 @@ function displayState() {
       }
     }else{
       for(l = 0; l < tempArray.length; l++){
-        // for(let q = 0; q < state.length; q++) {
-        //   if(state[q].value[0] === tempArray[l][0] && state[q].value[1] === tempArray[l][1] && state[q].value[2] === tempArray[l][2]) {
-        //     console.log("bhayeko" + tempArray[l]);
-        //     if(state[q].visited === true) {
-        //       fill(0, 255, 0);        
-        //       text(tempArray[l], ((state[i].x) - ((tempArray.length - 3) * 25) - 50) + l * 50, state[i].y + 40);
-
-        //     }else {
-        //       fill(255, 165, 0);  
-        //       text(tempArray[l], ((state[i].x) - ((tempArray.length - 3) * 25) - 50) + l * 50, state[i].y + 40);
-        //     }  
-        //   }
-        // }
+        for(let q = 0; q < state.length; q++) {
+          if(state[q].value[0] === tempArray[l][0] && state[q].value[1] === tempArray[l][1] && state[q].value[2] === tempArray[l][2]) {
+            console.log("bhayeko" + tempArray[l]);
+            if(state[q].visited === true) {
+              fill(0, 255, 0);        
+            }else {
+              fill(0, 0, 255);  
+            }  
+          }
+        }
+        stroke(255, 255, 255, 120);
+        line(state[i].x + 15, state[i].y + 5, ((state[i].x) - ((tempArray.length - 3) * 25) - 50) + l * 50 + 15, state[i].y + 20);
+        noStroke();
         text(tempArray[l], ((state[i].x) - ((tempArray.length - 3) * 25) - 50) + l * 50, state[i].y + 40);
-
-        // text(tempArray[l], ((windowWidth / 2) - ((tempArray.length - 3) * 25) - 50) + l * 50, 70 + i * 40);
+        fill(255, 0, 0);
         for(let b = 0; b < state.length; b++) {
           if(state[b].value[0] === tempArray[l][0] && state[b].value[1] === tempArray[l][1] && state[b].value[2] === tempArray[l][2]) {
             state[b].x =((state[i].x) - ((tempArray.length - 3) * 25) - 50) + l * 50, state[i].y + 40;
@@ -230,13 +219,10 @@ function displayState() {
         }
       }
     }
-    
   }
 }
 
 function tempChecker(value, goalArray) {
-  // console.log(goalArray);
-  // console.log(value);
   for(let i = 0; i < goalArray.length; i++) {
     if(goalArray[0] === value[0] && goalArray[1] === value[1] && goalArray[2] === value[2]) {
       return true;
