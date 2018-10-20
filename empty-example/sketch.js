ea7a8909c9fb2ad52b1c9c744dd7fa3a975e8871
Nadeem <shakya.nadim12@gmail.com>
2018-10-20 09:59:56 +0545

    minor code fixes

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -18,8 +18,8 @@ rootNode.value = initialState;
 rootNode.parent = initialState;
 rootNode.visited = false;
 
-
 function setup() {
+  frameRate(3);
   createCanvas(windowWidth, windowHeight);
   background(0, 0, 0, 200);
   stroke(255);
@@ -35,9 +35,104 @@ function setup() {
   console.log(state);
   console.log("Killed State");
   console.log(killedState);
-  displayState();
+  // displayState();
 }
+var i = 0;
+function draw() {
+  textSize(20);
+  fill(255, 165, 0);
+  noStroke();
+  text(state[0].value, windowWidth/2, 70);
+  textSize(16);
+  let tempArray = [];
+  for(j = i + 1; j < state.length; j++) {
+    if(state[j].parent[0] === state[i].value[0] && state[j].parent[1] === state[i].value[1] && state[j].parent[2] === state[i].value[2] ) {
+      if(!tempChecker(state[j].value, tempArray)) {
+        tempArray.push(state[j].value);
+      }
+    }
+  }
+  for(k = 0; k < killedState.length; k++) {
+    if(killedState[k].parent[0] === state[i].value[0] && killedState[k].parent[1] === state[i].value[1] && killedState[k].parent[2] === state[i].value[2] ) {
+      // console.log(killedStsate[k].value);
+      if(!tempChecker(killedState[k].value, tempArray)) {
+        tempArray.push(killedState[k].value);
+      }
+    } 
+  }    
+  if(tempArray.length === 1) {
+    for(let w = 0; w < state.length; w++) {
+      if(state[w].value[0] === tempArray[0][0] && state[w].value[1] === tempArray[0][1] && state[w].value[2] === tempArray[0][2]) {
+        if(state[w].visited) {
+          fill(0, 255, 0);            
+        }
+      }
+    }
+    stroke(255, 255, 255, 120);
 
+    line(state[i].x + 15, state[i].y, state[i].x + 15, state[i].y + 20);
+    noStroke();
+    text(tempArray[0], state[i].x, state[i].y + 40);
+    fill(255, 0, 0);
+    for(let b = 0; b < state.length; b++) {
+      if(state[b].value[0] === tempArray[0][0] && state[b].value[1] === tempArray[0][1] && state[b].value[2] === tempArray[0][2]) {
+        state[b].x =  state[i].x;
+        state[b].y = state[i].y + 50;
+      }
+    }       
+  }else if(tempArray.length !== 0 && tempArray.length % 2 === 0) {
+    for(p = 0; p < tempArray.length; p++) {
+      for(let q = 0; q < state.length; q++) {
+        if(state[q].value[0] === tempArray[p][0] && state[q].value[1] === tempArray[p][1] && state[q].value[2] === tempArray[p][2]) {
+          if(state[q].visited === true) {
+            fill(0, 255, 0);
+          }else { 
+            fill(255, 165, 0);
+          }  
+        }
+      }
+      stroke(255, 255, 255, 120);
+      line(state[i].x + 15, state[i].y, state[i].x - (25 * (tempArray.length - 1)) + p * 50 + 15, state[i].y + 20);
+      noStroke();
+      text(tempArray[p],(state[i].x - (25 * (tempArray.length - 1))) + p * 50, state[i].y + 40);
+      fill(255, 0, 0);
+
+      for(let b = 0; b < state.length; b++) {
+        if(state[b].value[0] === tempArray[p][0] && state[b].value[1] === tempArray[p][1] && state[b].value[2] === tempArray[p][2]) {
+          state[b].x = state[i].x - (25 * (tempArray.length - 1)) + p * 50, state[i].y + 40;
+          state[b].y = state[i].y + 50;
+        }
+      }        
+    }
+  }else{
+    for(l = 0; l < tempArray.length; l++){
+      for(let q = 0; q < state.length; q++) {
+        if(state[q].value[0] === tempArray[l][0] && state[q].value[1] === tempArray[l][1] && state[q].value[2] === tempArray[l][2]) {
+          if(state[q].visited === true) {
+            fill(0, 255, 0);        
+          }else {
+            fill(255, 165, 0);
+          }  
+        }
+      }
+      stroke(255, 255, 255, 120);
+      line(state[i].x + 15, state[i].y + 5, ((state[i].x) - ((tempArray.length - 3) * 25) - 50) + l * 50 + 15, state[i].y + 20);
+      noStroke();
+      text(tempArray[l], ((state[i].x) - ((tempArray.length - 3) * 25) - 50) + l * 50, state[i].y + 40);
+      fill(255, 0, 0);
+      for(let b = 0; b < state.length; b++) {
+        if(state[b].value[0] === tempArray[l][0] && state[b].value[1] === tempArray[l][1] && state[b].value[2] === tempArray[l][2]) {
+          state[b].x =((state[i].x) - ((tempArray.length - 3) * 25) - 50) + l * 50, state[i].y + 40;
+          state[b].y = state[i].y + 50;
+        }
+      }
+    }
+  }  
+  i++;
+  if(i >= state.length) {
+    noLoop();
+  }
+}
 function applyOperation(tempState) {
     if(tempState.visited === true) {
       state.splice(state.length - 1, 1);
@@ -173,7 +268,6 @@ function displayState() {
       for(p = 0; p < tempArray.length; p++) {
         for(let q = 0; q < state.length; q++) {
           if(state[q].value[0] === tempArray[p][0] && state[q].value[1] === tempArray[p][1] && state[q].value[2] === tempArray[p][2]) {
-            console.log("bhayeko" + tempArray[p]);
             if(state[q].visited === true) {
               fill(0, 255, 0);
             }else { 
